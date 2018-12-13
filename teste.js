59803694a6f10e07e7359a7b3b4627df2a171f95
Rodrigo F Martins <rodrigo.fonsecamartins@gmail.com>
2018-12-13 13:48:29 -0200

    Alteracao teste.js

--- teste.js
+++ teste.js
@@ -1,3 +1,19 @@
-lsdfjklasjd
-sldfjls
-sldjfklsj
+function calcular(){
+    operacao = document.getElementById("operacao").value;
+    valor1   = parseFloat(document.getElementById("num1").value);
+    valor2   = parseFloat(document.getElementById("num2").value);
+    resultado = 0.
+    if (operacao == "soma"){
+        resultado = valor1 + valor2;
+    }else if(operacao == "subtracao"){
+        resultado = valor1 - valor2;
+    }else if(operacao == "multiplicacao"){
+        resultado = valor1 * valor2;    
+    }else if(operacao == "divisao"){
+        resultado = valor1 / valor2;
+    }
+    alert(resultado);
+}
+function teste(){
+    
+}
